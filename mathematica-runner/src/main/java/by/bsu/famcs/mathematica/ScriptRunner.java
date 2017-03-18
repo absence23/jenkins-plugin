1c35332d697f557e0844e5ee1f486fdf182d099c
absence23 <loveless1>
2017-03-18 14:11:34 +0300

    Add run and test methods

--- mathematica-runner/src/main/java/by/bsu/famcs/mathematica/ScriptRunner.java
+++ mathematica-runner/src/main/java/by/bsu/famcs/mathematica/ScriptRunner.java
@@ -2,29 +2,58 @@ package by.bsu.famcs.mathematica;
 
 import com.wolfram.jlink.*;
 
+import java.util.StringTokenizer;
+
 /**
  * Run Wolfram Mathematica scripts
  */
-public class ScriptRunner {
-
-    private static final String KERNEL_LINK = "-linkmode launch -linkname '%s'";
+public class ScriptRunner implements AutoCloseable{
+    private static final String KERNEL_LINK = "\\MathKernel.exe";
+    private static final String JLINK_PATH = "\\SystemFiles\\Links\\JLink";
+    private static final String LAUNCH_LINK = "-linkmode launch -linkname '%s'";
     private static ScriptRunner scriptRunner;
-    private String kernelPath;
-    private String jLinkPath;
+    private String mathematicaHome;
     private KernelLink kernelLink;
 
-    private ScriptRunner(String kernelPath, String jLinkPath) throws MathLinkException {
-        this.kernelPath = kernelPath;
-        this.jLinkPath = jLinkPath;
-        kernelLink = MathLinkFactory.createKernelLink(String.format(KERNEL_LINK, kernelPath));
-        System.setProperty("com.wolfram.jlink.libdir", jLinkPath);
+    public ScriptRunner(String mathematicaHome) throws MathLinkException {
+        this.mathematicaHome = mathematicaHome;
+        System.setProperty("com.wolfram.jlink.libdir", mathematicaHome + JLINK_PATH);
+        kernelLink = MathLinkFactory.createKernelLink(String.format(LAUNCH_LINK, mathematicaHome + KERNEL_LINK));
     }
 
-    public static ScriptRunner getInstance() {
-        return scriptRunner;
+    public void run(String dir, String filename) throws MathLinkException {
+        kernelLink.discardAnswer();
+        String expr = "UsingFrontEnd[NotebookImport[ToFileName[\"" + dir + "\", \"" + filename + "\"], \"Input\"->\"Text\"]]";
+        kernelLink.evaluate(expr);
+        kernelLink.waitForAnswer();
+        Expr expression = kernelLink.getExpr();
+        System.out.println(expression.toString());
+        String x = expression.toString();
+        x = x.substring(2, x.length() - 2).replace("\r\n", "");
+        System.out.println("Result = " + x);
+        System.out.println("====================");
+        StringTokenizer tokenizer = new StringTokenizer(x, ";", false);
+        String token;
+        while (tokenizer.hasMoreTokens()) {
+            token = tokenizer.nextToken();
+            kernelLink.evaluate("ToExpression[" + token + "]");
+            kernelLink.waitForAnswer();
+            System.out.println(kernelLink.getExpr().toString());
+        }
     }
 
-    public static void init(String kernelPath, String jLinkPath) throws MathLinkException {
-        scriptRunner = new ScriptRunner(kernelPath, jLinkPath);
+    public boolean test(String dir, String filename, String value) throws MathLinkException{
+        String expr = "UsingFrontEnd[NotebookEvaluate[ToFileName[\"" + dir + "\", \"" + filename + "\"]]]";
+        kernelLink.discardAnswer();
+        kernelLink.evaluate(expr);
+        kernelLink.waitForAnswer();
+        Expr expression = kernelLink.getExpr();
+        System.out.println("Result = " + expression.toString());
+        kernelLink.close();
+        return expression.toString().equals(value);
+    }
+
+    public void close() {
+        kernelLink.close();
     }
 }
--- mathematica-runner/src/test/java/by/bsu/famcs/mathematica/RunnerTest.java
+++ mathematica-runner/src/test/java/by/bsu/famcs/mathematica/RunnerTest.java
@@ -4,11 +4,18 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RunnerTest {
+    private static final String MATHEMATICA_HOME = "D:\\InstalledSoft\\Utils\\WolframMathematica";
 
     @Test
-    public void initTest() throws Exception {
-        ScriptRunner.init("", "");
-        ScriptRunner scriptRunner = ScriptRunner.getInstance();
-        Assert.assertNotNull(scriptRunner);
+    public void runTest() throws Exception {
+        ScriptRunner scriptRunner = new ScriptRunner(MATHEMATICA_HOME);
+        scriptRunner.run("D:\\Documents", "test.nb");
+        scriptRunner.close();
+    }
+    @Test
+    public void testTest() throws Exception {
+        ScriptRunner scriptRunner = new ScriptRunner(MATHEMATICA_HOME);
+        Assert.assertTrue(scriptRunner.test("D:\\Documents", "test.nb", "4"));
+        scriptRunner.close();
     }
 }
